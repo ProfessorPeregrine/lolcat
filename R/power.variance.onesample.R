67d86bc5234419ac1edbef6f5a16c5f96da73428
ProfessorPeregrine <92055623+ProfessorPeregrine@users.noreply.github.com>
2026-09-25 16:13:48 -0600

    Correct power calculation in power.variance.onesample
    Fix calculation of power for two-sided hypothesis tests by correctly using both critical chi-square values.

--- R/power.variance.onesample.R
+++ R/power.variance.onesample.R
@@ -1,118 +1,57 @@
-power.variance.onesample <- function(
-  sample.size = 1
-  ,null.hypothesis.variance = 1
-  ,alternative.hypothesis.variance = 2
-  ,alpha = .05
-  ,alternative = c("two.sided","greater", "less")
-  ,details = TRUE
-  
-  
-) {
+# -----------------------------------------------------------------------------
+# power.variance.onesample
+# Bug: two.sided beta used only the critical chi-square on the "near" side of
+#      the ratio. Both chi.lower and chi.upper are already computed.
+# Fix: power = P(X <= chi.lower | H1) + P(X > chi.upper | H1)
+#      under the scaled gamma used by lolcat (shape=(n-1)/2, scale=2*ratio).
+# -----------------------------------------------------------------------------
+power.variance.onesample <- function(sample.size = 1,
+                                     null.hypothesis.variance = 1,
+                                     alternative.hypothesis.variance = 2,
+                                     alpha = 0.05,
+                                     alternative = c("two.sided", "greater", "less"),
+                                     details = TRUE) {
   validate.htest.alternative(alternative = alternative)
-  ratio <- alternative.hypothesis.variance/null.hypothesis.variance
-  #ratio <- null.hypothesis.variance/alternative.hypothesis.variance # Don't do this
+  ratio <- alternative.hypothesis.variance / null.hypothesis.variance
   n <- sample.size
-
-  # Find Rejection Regions
-  
-  chi.upper <- NA
-  chi.lower <- NA
-  
   if (alternative[1] == "two.sided") {
-    chi.lower <- qchisq(alpha/2, n-1, lower.tail = T)
-    chi.upper <- qchisq(1-alpha/2, n-1, lower.tail = T)
+    chi.lower <- qchisq(alpha / 2, n - 1, lower.tail = TRUE)
+    chi.upper <- qchisq(1 - alpha / 2, n - 1, lower.tail = TRUE)
   } else {
-    chi.lower <- qchisq(alpha, n-1, lower.tail = T)
-    chi.upper <- qchisq(1-alpha, n-1, lower.tail = T)
+    chi.lower <- qchisq(alpha, n - 1, lower.tail = TRUE)
+    chi.upper <- qchisq(1 - alpha, n - 1, lower.tail = TRUE)
   }
-  
-  #print(paste(chi.lower,chi.upper))
-  
-  # Determine Alternative Hypothesis Distribution
-  #n.scaled <- (n-1)*ratio
-  
-  p.fn <- function(q, lower.tail = T) {
-    pgamma(q = q, shape = (n-1)/2, scale= 2*ratio, lower.tail = lower.tail)# pchisq(df = n.scaled, lower.tail = lower.tail)
+  p.fn <- function(q, lower.tail = TRUE) {
+    pgamma(q = q, shape = (n - 1) / 2, scale = 2 * ratio, lower.tail = lower.tail)
   }
-  
-  beta <- NA
-  
-  # Calculate beta
-  if (ratio < 1) {
-    if (alternative[1] == "two.sided") {
-      beta <- p.fn(chi.lower, lower.tail = F)
-    } else if (alternative[1] == "less") {
-      beta <- p.fn(chi.lower, lower.tail = F)
-    } else {
-      beta <- p.fn(chi.upper, lower.tail = T)
-    }
-    
-  } else if (ratio > 1) {
-    if (alternative[1] == "two.sided") {
-      beta <- p.fn(chi.upper, lower.tail = T)
-    } else if (alternative[1] == "less") {
-      beta <- p.fn(chi.lower, lower.tail = F)
-    } else {
-      beta <- p.fn(chi.upper, lower.tail = T)
-    }
-  } else {
+
+  if (ratio == 1) {
     beta <- 1
+  } else if (alternative[1] == "two.sided") {
+    # both limbs
+    pow <- p.fn(chi.lower, lower.tail = TRUE) + p.fn(chi.upper, lower.tail = FALSE)
+    beta <- 1 - pow
+  } else if (ratio < 1) {
+    if (alternative[1] == "less") {
+      beta <- p.fn(chi.lower, lower.tail = FALSE)
+    } else {
+      beta <- p.fn(chi.upper, lower.tail = TRUE)
+    }
+  } else {
+    if (alternative[1] == "less") {
+      beta <- p.fn(chi.lower, lower.tail = FALSE)
+    } else {
+      beta <- p.fn(chi.upper, lower.tail = TRUE)
+    }
   }
-  
-  pow <- 1 - beta  
-  
+  pow <- 1 - beta
   if (details) {
-    as.data.frame(list(test="chi-square"
-                       ,type = "one.sample"
-                       ,alternative = alternative[1]
-                       ,sample.size = n
-                       ,df = n - 1
-                       ,ratio = ratio
-                       ,alpha = alpha
-                       ,conf.level = 1-alpha
-                       ,beta = beta
-                       ,power = 1- beta
+    as.data.frame(list(
+      test = "chi-square", type = "one.sample", alternative = alternative[1],
+      sample.size = n, df = n - 1, ratio = ratio, alpha = alpha,
+      conf.level = 1 - alpha, beta = beta, power = pow
     ))
-    
-  }
-  else {
+  } else {
     pow
   }
-  
-  
 }
-
-# power.variance.onesample(
-#   sample.size = 21
-#   ,alternative.hypothesis.variance = 2
-#   ,null.hypothesis.variance = 1
-#   ,alpha = .05
-#   ,alternative = "two.sided"
-# )
-
-# pgamma(34.1696069, 10, scale = 4, lower.tail = F)
-# 
-# 
-# x <- seq(0,50,.01)
-# y <- dgamma(x, shape = 10, scale = 4)
-# 
-# plot(x,y,type="l",col="blue")
-# 
-# y2 <- dchisq(x, df = 20)
-# 
-# lines(x,y2,col="orange")
-# 
-# curve(function(x) {dgamma(x, 10, 2)}, 0,50)
-# 
-# ?dgamma
-# 
-# 
-# ?pchisq
-# 
-# d<-data.frame(n = 20:50)
-# 
-# d$pow <-sapply(d$n, FUN=function(n) {
-#   pchisq(34.1696069, n, lower.tail = F)  
-# })
-# 
-# d
