022cf2a20b494500bc6adbed38d1de1e293d81cf
ProfessorPeregrine <92055623+ProfessorPeregrine@users.noreply.github.com>
2026-09-25 16:18:13 -0600

    Fix two-sided power calculation in Poisson test
    Refactor power.count.poisson.onesample.exact function to fix two-sided power calculation by summing both tails under H1. Update parameter handling and return structure for clarity.

--- R/power.count.poisson.onesample.exact.R
+++ R/power.count.poisson.onesample.exact.R
@@ -1,107 +1,60 @@
-#' Power - Single Sample Poisson Test  
-#' 
-#' Power calculation utilizes the Poisson distribution ("exact" sample size/power).
-#'
-#' @param sample.size Scalar - sample size 
-#' @param lambda.null.hypothesis Scalar - null hypothesis lambda parameter
-#' @param lambda.alternative.hypothesis Scalar - alternative hypothesis lambda parameter
-#' @param alpha Scalar - Type I error rate
-#' @param alternative Scalar (character) - alternative hypothesis 
-#' @param details Logical - Return calculation details (default) or return only power (details = FALSE)
-#'
-#' @return A data frame with details about the calculation or a single value with power (details = F). 
-
+# -----------------------------------------------------------------------------
+# power.count.poisson.onesample.exact
+# Bug: two.sided recursively calls one-sided at alpha/2 in only one direction.
+# Fix: both Poisson critical counts under H0, sum both tails under H1.
+#      (Uses qpois; equivalent in spirit to summing both one-sided limbs.)
+# -----------------------------------------------------------------------------
 power.count.poisson.onesample.exact <- function(
-  sample.size
-  ,lambda.null.hypothesis
-  ,lambda.alternative.hypothesis
-  ,alpha = .05
-  ,alternative = c("two.sided","less","greater")
-  ,details = T
-) {
+    sample.size, lambda.null.hypothesis, lambda.alternative.hypothesis,
+    alpha = 0.05, alternative = c("two.sided", "less", "greater"), details = TRUE) {
   validate.htest.alternative(alternative = alternative)
-
+  n <- sample.size
+  lambda0 <- lambda.null.hypothesis
+  lambda1 <- lambda.alternative.hypothesis
   pow <- NA
+  alpha_out <- alpha
 
   if (alternative[1] == "two.sided") {
-    if (lambda.alternative.hypothesis == lambda.null.hypothesis) {
-      #NA
-    } else {
-      a.sample.size = sample.size
-      a.lambda.null.hypothesis = lambda.null.hypothesis
-      a.lambda.alternative.hypothesis = lambda.alternative.hypothesis
-      a.alpha = alpha
-      a.alternative = if(lambda.alternative.hypothesis < lambda.null.hypothesis) {
-          "less"
-        } else {
-          "greater"
-        }
-
-      pow <- power.count.poisson.onesample.exact(
-        sample.size = a.sample.size
-        ,lambda.null.hypothesis = a.lambda.null.hypothesis
-        ,lambda.alternative.hypothesis = a.lambda.alternative.hypothesis
-        ,alpha = a.alpha/2
-        ,alternative = a.alternative
-        ,details = T
-      )
-      
-      alpha <- 2*rmnames(pow$alpha[1])
-      pow   <- rmnames(pow$power[1])
+    if (lambda1 != lambda0) {
+      crit_x_l <- qpois(p = alpha / 2, lambda = n * lambda0, lower.tail = TRUE) - 1
+      crit_x_u <- qpois(p = 1 - (alpha / 2), lambda = n * lambda0, lower.tail = TRUE) + 1
+      alpha_out <- ppois(q = crit_x_l, lambda = n * lambda0, lower.tail = TRUE) +
+        ppois(q = crit_x_u - 1, lambda = n * lambda0, lower.tail = FALSE)
+      pow <- ppois(q = crit_x_l, lambda = n * lambda1, lower.tail = TRUE) +
+        ppois(q = crit_x_u - 1, lambda = n * lambda1, lower.tail = FALSE)
     }
   } else if (alternative[1] == "greater") {
-    if (lambda.alternative.hypothesis <= lambda.null.hypothesis) {
-      #NA
-    } else {
-      table.lambda.alternative.hypothesis <- table.dist.poisson(
-        lambda = sample.size*lambda.alternative.hypothesis
-      )
-      table.lambda.null.hypothesis        <- table.dist.poisson(
-        lambda = sample.size*lambda.null.hypothesis,
+    if (lambda1 > lambda0) {
+      table.lambda.alternative.hypothesis <- table.dist.poisson(lambda = n * lambda1)
+      table.lambda.null.hypothesis <- table.dist.poisson(
+        lambda = n * lambda0,
         include.x = nrow(table.lambda.alternative.hypothesis)
       )
-
-      idx   <- which(table.lambda.null.hypothesis$eq.and.above <= alpha)
-      pow   <- table.lambda.alternative.hypothesis$eq.and.above[min(idx)+1]
-      alpha <- table.lambda.null.hypothesis$eq.and.above[min(idx)]
+      idx <- which(table.lambda.null.hypothesis$eq.and.above <= alpha)
+      pow <- table.lambda.alternative.hypothesis$eq.and.above[min(idx) + 1]
+      alpha_out <- table.lambda.null.hypothesis$eq.and.above[min(idx)]
     }
   } else if (alternative[1] == "less") {
-    if (lambda.alternative.hypothesis >= lambda.null.hypothesis) {
-      #NA
-    } else {
-      table.lambda.null.hypothesis        <- table.dist.poisson(
-        lambda = sample.size*lambda.null.hypothesis
-      )
+    if (lambda1 < lambda0) {
+      table.lambda.null.hypothesis <- table.dist.poisson(lambda = n * lambda0)
       table.lambda.alternative.hypothesis <- table.dist.poisson(
-        lambda = sample.size*lambda.alternative.hypothesis,
+        lambda = n * lambda1,
         include.x = nrow(table.lambda.null.hypothesis)
       )
-
-      idx   <- which(table.lambda.null.hypothesis$eq.and.below <= alpha)
-      alpha <- table.lambda.null.hypothesis$eq.and.below[length(idx)]
-      pow   <- table.lambda.alternative.hypothesis$eq.and.below[length(idx)-1]
+      idx <- which(table.lambda.null.hypothesis$eq.and.below <= alpha)
+      alpha_out <- table.lambda.null.hypothesis$eq.and.below[length(idx)]
+      pow <- table.lambda.alternative.hypothesis$eq.and.below[length(idx) - 1]
     }
   }
-  
-  beta <- 1-pow
-  
+  beta <- 1 - pow
   if (details) {
-    as.data.frame(list(test="poisson"
-                       ,type = "one.sample"
-                       ,alternative = alternative[1]
-                       ,sample.size = sample.size
-                       ,actual = sample.size
-                       ,lambda.null = lambda.null.hypothesis
-                       ,lambda.alternative = lambda.alternative.hypothesis
-                       ,alpha = alpha
-                       ,conf.level = 1-alpha
-                       ,beta = beta
-                       ,power = pow
+    as.data.frame(list(
+      test = "poisson", type = "one.sample", alternative = alternative[1],
+      sample.size = sample.size, actual = sample.size,
+      lambda.null = lambda0, lambda.alternative = lambda1,
+      alpha = alpha_out, conf.level = 1 - alpha_out, beta = beta, power = pow
     ))
-    
-  }
-  else {
+  } else {
     pow
   }
-  
 }
