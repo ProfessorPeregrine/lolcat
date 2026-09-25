e30e3d1534b1b54a7bbf931285caf008264a1793
ProfessorPeregrine <92055623+ProfessorPeregrine@users.noreply.github.com>
2026-09-25 16:16:43 -0600

    Fix power calculation in two-sample proportion test
    Fixed the calculation of power for two-sample proportion tests by correcting the standard error calculations and handling alternative hypotheses properly.

--- R/power.proportion.test.twosample.approximate.R
+++ R/power.proportion.test.twosample.approximate.R
@@ -1,49 +1,42 @@
-power.proportion.test.twosample.approximate <- function(sample.size
-                                                        ,proportion.g1
-                                                        ,proportion.g2
-                                                        ,alpha = .05
-                                                        ,alternative = c("two.sided","less","greater")
-                                                        ,details = T
-) {
+# -----------------------------------------------------------------------------
+# power.proportion.test.twosample.approximate
+# Bug: same one-limb Phi(z_beta) pattern; mismatched one-sided → NA.
+# Fix: both limbs with lolcat SE convention (se_null = sqrt(2*p1(1-p1)),
+#      se_alt = sqrt(p1(1-p1)+p2(1-p2)), equal n per group).
+# -----------------------------------------------------------------------------
+power.proportion.test.twosample.approximate <- function(
+    sample.size, proportion.g1, proportion.g2, alpha = 0.05,
+    alternative = c("two.sided", "less", "greater"), details = TRUE) {
   validate.htest.alternative(alternative = alternative)
-  
   n <- sample.size
-  
-  z_alpha <- qnorm(ifelse(alternative[1] == "two.sided",alpha/2,alpha), lower.tail = F)
-  
-  se.g1        <- proportion.g1*(1-proportion.g1)
-  se.g2        <- proportion.g2*(1-proportion.g2)
-  
-  z_beta <- sqrt(n)*abs(proportion.g1 - proportion.g2) - z_alpha*sqrt(2*se.g1)
-  z_beta <- z_beta / sqrt(se.g1 + se.g2)
-  
-  pow <- pnorm(z_beta, lower.tail = T)
-  
-  if (alternative[1] == "less" & proportion.g1 > proportion.g2) {
-    pow <- NA
-  } else if (alternative[1] == "greater" & proportion.g1 < proportion.g2) {
-    pow <- NA
+  se.g1 <- proportion.g1 * (1 - proportion.g1)
+  se.g2 <- proportion.g2 * (1 - proportion.g2)
+  se_null <- sqrt(2 * se.g1)
+  se_alt <- sqrt(se.g1 + se.g2)
+  mu <- sqrt(n) * abs(proportion.g1 - proportion.g2)
+
+  if (alternative[1] == "two.sided") {
+    z_a <- qnorm(alpha / 2, lower.tail = FALSE)
+    pow <- pnorm((mu - z_a * se_null) / se_alt) +
+      pnorm((-z_a * se_null - mu) / se_alt)
+  } else {
+    z_alpha <- qnorm(alpha, lower.tail = FALSE)
+    z_beta <- (mu - z_alpha * se_null) / se_alt
+    pow <- pnorm(z_beta, lower.tail = TRUE)
+    if (alternative[1] == "less" && proportion.g1 > proportion.g2) {
+      pow <- NA
+    } else if (alternative[1] == "greater" && proportion.g1 < proportion.g2) {
+      pow <- NA
+    }
   }
-  
   if (details) {
-    as.data.frame(list(test="proportion"
-                       ,type = "two.sample"
-                       ,alternative = alternative[1]
-                       ,sample.size = sample.size
-                       ,actual = sample.size
-                       ,proportion.g1 = proportion.g1
-                       ,proportion.g2 = proportion.g2
-                       ,alpha = alpha
-                       ,conf.level = 1-alpha
-                       ,beta = 1-pow
-                       ,power = pow
+    as.data.frame(list(
+      test = "proportion", type = "two.sample", alternative = alternative[1],
+      sample.size = sample.size, actual = sample.size,
+      proportion.g1 = proportion.g1, proportion.g2 = proportion.g2,
+      alpha = alpha, conf.level = 1 - alpha, beta = 1 - pow, power = pow
     ))
-    
-  }
-  else {
+  } else {
     pow
   }
-  
-  
-  
 }
